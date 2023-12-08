626142c69b66926c62b396930120ff08cd7afc1a
dlaytonstl <dlaytonstl@gmail.com>
2023-12-08 16:05:29 -0500

    second commit

--- models.py
+++ models.py
@@ -1,9 +1,24 @@
 #models.py
 
 from datetime import datetime
-
+from marshmallow_sqlalchemy import fields
 from config import db, ma
 
+class Note(db.Model):
+    __tablename__ = "note"
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+class NoteSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Note
+        load_instance = True
+        sqla_session = db.session
+        include_fk = True
 
 class Person(db.Model):
     __tablename__ = "person"
@@ -13,6 +28,13 @@ class Person(db.Model):
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+    notes = db.relationship(
+        Note,
+        backref="person",
+        cascade="all, delete, delete-orphan",
+        single_parent=True,
+        order_by="desc(Note.timestamp)"
+    )
 
 
 class PersonSchema(ma.SQLAlchemyAutoSchema):
@@ -20,8 +42,11 @@ class PersonSchema(ma.SQLAlchemyAutoSchema):
         model = Person
         load_instance = True
         sqla_session = db.session
+        include_relationships = True
 
+    notes = fields.Nested(NoteSchema, many=True)
 
+note_schema = NoteSchema()
 person_schema = PersonSchema()
 people_schema = PersonSchema(many=True)
 
